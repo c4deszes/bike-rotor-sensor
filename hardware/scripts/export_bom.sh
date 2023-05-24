73e5f565742126ac2f47d8a47dc17cb2856e69b4
Balazs Eszes <c4deszes@gmail.com>
2023-05-24 09:12:36 +0200

    Kicad7 sch file extension update

--- hardware/scripts/export_bom.sh
+++ hardware/scripts/export_bom.sh
@@ -1,6 +1,6 @@
 kibot \
 -b design/rotor-sensor/rotor-sensor.kicad_pcb \
--e design/rotor-sensor/rotor-sensor.sch \
+-e design/rotor-sensor/rotor-sensor.kicad_sch \
 -c config/outputs.yaml \
 -g variant=$VARIANT \
 bom
--- hardware/scripts/run_erc.sh
+++ hardware/scripts/run_erc.sh
@@ -1,1 +1,1 @@
-kibot -e design/rotor-sensor/rotor-sensor.sch -c config/erc.yaml
+kibot -e design/rotor-sensor/rotor-sensor.kicad_sch -c config/erc.yaml
